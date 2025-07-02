448b56ab2d77f9aed648ed6aad7b0c9ed2323c5f
Carlos Ceron <97488732+lysein@users.noreply.github.com>
2025-07-01 21:41:38 -0600

    cors and new port

--- src/main.py
+++ src/main.py
@@ -3,15 +3,26 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.routers import auth
 from src.api.routers import dev_auth
 from src.api.routers import matchmaking
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ⬅️ TEMPORARY: lets any origin connect
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router, prefix="/auth")
 app.include_router(dev_auth.router, prefix="/dev-auth")
 app.include_router(matchmaking.router, prefix="/matchmaking")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="localhost", port=8080)
